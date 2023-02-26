9f0878040d5ecf865787d3967e69631288742154
NaokiM03 <naokim03.lib+dev@gmail.com>
2023-02-26 14:53:50 +0900

    Refactor

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,11 @@ import * as vscode from "vscode";
 
 import * as TOML from "@iarna/toml";
 
+type Line = {
+  content: string;
+  number: number;
+};
+
 type RunPkgArg = {
   filePath: string;
   pkg: string;
@@ -14,83 +19,64 @@ class CodeLensProvider implements vscode.CodeLensProvider {
     document: vscode.TextDocument,
     _token: vscode.CancellationToken
   ): vscode.ProviderResult<vscode.CodeLens[]> {
-    const fileContent: string = document.getText();
+    return document
+      .getText()
+      .split("\n")
+      .map((s: string, i: number) => ({ content: s, number: i }))
+      .reduce((acc: Line[][], v: Line, i, lines: Line[]) => {
+        if (i === 0) {
+          acc.push([v]);
+          return acc;
+        }
 
-    const lines: {
-      content: string;
-      number: number;
-    }[] = fileContent.split("\n").map((s: string, i: number) => {
-      return { content: s, number: i };
-    });
+        if (v.content.startsWith("//# ---")) {
+          acc.push([]);
+          return acc;
+        }
 
-    if (lines.length === 0) return;
+        if (lines[i - 1].content.startsWith("//# ---") && v.content === "") {
+          return acc;
+        }
 
-    const targetLines: {
-      content: string;
-      number: number;
-    }[] = lines.filter(
-      (line) => line.content.match(/^fn main\(\)+\s/) !== null
-    );
+        acc[acc.length - 1].push(v);
+        return acc;
+      }, [])
+      .map((lines: Line[]) => {
+        let toml: TOML.JsonMap;
+        try {
+          const t: string = lines
+            .filter((line) => line.content.startsWith("//# "))
+            .map((line) => line.content.substring(4))
+            .join("\n");
+          toml = TOML.parse(t);
+        } catch (e) {
+          throw e;
+        }
 
-    if (targetLines.length === 0) return;
+        const pkgField = toml.package as TOML.JsonMap;
+        const pkgName = pkgField.name as string;
 
-    const ranges: vscode.Range[] = targetLines.map(
-      (line) =>
-        new vscode.Range(
-          new vscode.Position(line.number, 0),
-          new vscode.Position(line.number, line.content.length)
-        )
-    );
+        const targetLine = lines.find(
+          (line) => line.content.match(/^fn main\(\)+\s/) !== null
+        ) as Line;
 
-    const pkgs: string[] = fileContent.split("//# ---");
-    // if (targetLines.length !== pkgs.length) {}
+        const range = new vscode.Range(
+          new vscode.Position(targetLine.number, 0),
+          new vscode.Position(targetLine.number, targetLine.content.length)
+        );
 
-    const lineNumbers = pkgs.map((pkg, index, array) =>
-      index !== array.length - 1
-        ? pkg.split("\n").length - 1
-        : pkg.split("\n").length
-    );
-    for (let i = 0; i < lineNumbers.length; i++) {
-      if (i === 0) {
-        lineNumbers[i] = lineNumbers[i];
-      } else {
-        lineNumbers[i] = lineNumbers[i - 1] + lineNumbers[i];
-      }
-    }
+        const arg: RunPkgArg = {
+          filePath: document.fileName,
+          pkg: pkgName,
+        };
 
-    return ranges.map((range) => {
-      let index = lineNumbers.findIndex(
-        (lineNumber) => lineNumber > range.start.line + 1
-      );
-      const pkg = pkgs[index];
-
-      let toml: TOML.JsonMap;
-      try {
-        const t: string = pkg
-          .split("\n")
-          .filter((line) => line.startsWith("//# "))
-          .map((line) => line.substring(4))
-          .join("\n");
-        toml = TOML.parse(t);
-      } catch (e) {
-        throw e;
-      }
-
-      const pkgField = toml.package as TOML.JsonMap;
-      const pkgName = pkgField.name as string;
-
-      const arg: RunPkgArg = {
-        filePath: document.fileName,
-        pkg: pkgName,
-      };
-
-      return new vscode.CodeLens(range, {
-        title: "▶ Run",
-        command: "rspit.runPkg",
-        tooltip: `Run ${pkgName} package`,
-        arguments: [arg],
+        return new vscode.CodeLens(range, {
+          title: "▶ Run",
+          command: "rspit.runPkg",
+          tooltip: `Run ${pkgName} package`,
+          arguments: [arg],
+        });
       });
-    });
   }
 }
 
