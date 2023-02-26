8b27eba887e7778a0d23bf74e3e686d2292b745f
NaokiM03 <naokim03.lib+dev@gmail.com>
2023-02-27 03:37:09 +0900

    Add PACKAGES to Tree View

--- src/tree.ts
+++ src/tree.ts
@@ -1,7 +1,61 @@
+import * as path from "path";
+import * as fs from "fs";
+
 import * as vscode from "vscode";
+
+import * as TOML from "@iarna/toml";
 
 class Pkg extends vscode.TreeItem {
   iconPath = new vscode.ThemeIcon("package");
+}
+
+type Line = {
+  content: string;
+  number: number;
+};
+
+const toLine = (s: string, i: number): Line => ({ content: s, number: i });
+
+const splitLinesByPkg = (
+  acc: Line[][],
+  x: Line,
+  i: number,
+  lines: Line[]
+): Line[][] => {
+  if (i === 0) {
+    acc.push([]);
+    acc[acc.length - 1].push(x);
+    return acc;
+  }
+
+  if (x.content.startsWith("//# ---")) {
+    acc.push([]);
+    return acc;
+  }
+
+  if (lines[i - 1].content.startsWith("//# ---") && x.content === "") {
+    return acc;
+  }
+
+  acc[acc.length - 1].push(x);
+  return acc;
+};
+
+function extractPkgName(lines: Line[]): string {
+  const t: string = lines
+    .filter((line) => line.content.startsWith("//# "))
+    .map((line) => line.content.substring(4))
+    .join("\n");
+
+  let toml: TOML.JsonMap;
+  try {
+    toml = TOML.parse(t);
+  } catch (e) {
+    throw e;
+  }
+
+  const pkgField = toml.package as TOML.JsonMap;
+  return pkgField.name as string;
 }
 
 class PkgTreeProvider implements vscode.TreeDataProvider<Pkg> {
@@ -10,7 +64,29 @@ class PkgTreeProvider implements vscode.TreeDataProvider<Pkg> {
   }
 
   getChildren(_element?: Pkg): vscode.ProviderResult<Pkg[]> {
-    return [new Pkg("foo")];
+    const textEditor = vscode.window.activeTextEditor;
+    if (textEditor === undefined) {
+      return Promise.resolve([]);
+    }
+    if (!textEditor.document.fileName.endsWith("rspit.rs")) {
+      return Promise.resolve([]);
+    }
+
+    const dirPath = vscode.workspace
+      .getConfiguration("rspit")
+      .get("filePath") as string;
+    const filePath = path.join(dirPath, "rspit.rs");
+    const snippets = fs.readFileSync(filePath, "utf8");
+
+    return snippets
+      .split("\n")
+      .map(toLine)
+      .reduce(splitLinesByPkg, [])
+      .map((lines: Line[]) => {
+        const pkgName = extractPkgName(lines);
+        console.log(pkgName);
+        return new Pkg(pkgName);
+      });
   }
 }
 
